61d43ca44bd05bda0cdbd5e9402241d508397ede
bonganimasemola <bonganimasemola@icloud.com>
2023-11-22 19:53:04 +0300

    comprehensions and genrerators complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for i in num_list:
+        if i % 2 == 0:
+            even_numbers.append(i)
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
